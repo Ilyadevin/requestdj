946839750010bc1b9b2ea951a6280462ca782f6a
Ilya Erlingas <Aligruit@gmail.com>
2020-04-28 20:11:36 +0300

    correction

--- landing/app/views.py
+++ landing/app/views.py
@@ -4,35 +4,37 @@ from django.shortcuts import render_to_response
 
 counter_show = Counter()
 counter_click = Counter()
+template_original = 'landing.html'
+template_test = 'app/landing_alternate.html'
 
 
 def index(request):
-    global counter_show
+    global counter_show, counter_click
     if 'from-landing' in request.GET['from-landing']:
         counter_show['from-landing'] += 1
         counter_click['from-landing'] += 1
     else:
         pass
-    return render_to_response('index.html'), counter_click, counter_show
+    return render_to_response('index.html')
 
 
 def landing(request):
-    template_original = None
-    template_test = None
+    global counter_show, counter_click
+
     if 'ab-test-arg' in request.GET['app/landing_alternate.html' or template_original]:
-        template_test = 'app/landing_alternate.html'
-        template_original = 'landing.html'
         counter_show[template_test] += 1
-        counter_click['ab-test-arg'] += 1
-    return render_to_response(template_original, template_test), counter_click, counter_show
+        counter_click[template_test] += 1
+    else:
+        counter_click[template_original] += 1
+        counter_show[template_original] += 1
+    return render_to_response(template_original, template_test)
 
 
 def stats(request):
-    # Реализуйте логику подсчета отношения количества переходов к количеству показов страницы
-    # Чтобы отличить с какой версии лендинга был переход
-    # проверяйте GET параметр marker который может принимать значения test и original
-    # Для вывода результат передайте в следующем формате:
-    return render_to_response('stats.html', context={
-        'test_conversion': 0.5,
-        'original_conversion': 0.4,
-    })
+    original = counter_click[template_original] / counter_show[template_original]
+    test = counter_click[template_test] / counter_show[template_test]
+    if original or test in request.GET('marker'):
+        return render_to_response('stats.html', context={
+            'test_conversion': 0.5,
+            'original_conversion': 0.4,
+        })
