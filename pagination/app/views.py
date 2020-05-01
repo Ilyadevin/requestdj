e48b1bf0407383f3de1ed9d29c24d1d9da5d9a48
Ilya Erlingas <Aligruit@gmail.com>
2020-05-01 18:01:19 +0300

    +adding pagination

--- pagination/app/views.py
+++ pagination/app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response, redirect
 from django.urls import reverse
+import csv
 
 
 def index(request):
@@ -7,12 +8,14 @@ def index(request):
 
 
 def bus_stations(request):
-    current_page = 1
-    next_page_url = 'write your url'
-    return render_to_response('index.html', context={
-        'bus_stations': [{'Name': 'название', 'Street': 'улица', 'District': 'район'}],
-        'current_page': current_page,
-        'prev_page_url': None,
-        'next_page_url': next_page_url,
-    })
-
+    with open('data-398-2018-08-30.csv', 'r', encoding='utf8') as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            current_page = 1
+            next_page_url = 'write your url'
+            return render_to_response('index.html', context={
+                'bus_stations': [{'Name': row['StationName'], 'Street': row['Street'], 'District': row['District']}],
+                'current_page': current_page,
+                'prev_page_url': None,
+                'next_page_url': next_page_url,
+            })
