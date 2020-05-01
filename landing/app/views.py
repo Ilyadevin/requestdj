545cf89eda276958d6ad91ac1c9d5e7a387a8af2
Ilya Erlingas <Aligruit@gmail.com>
2020-05-01 18:00:58 +0300

    +correction landing

--- landing/app/views.py
+++ landing/app/views.py
@@ -9,32 +9,31 @@ template_test = 'app/landing_alternate.html'
 
 
 def index(request):
-    global counter_show, counter_click
     if 'from-landing' in request.GET['from-landing']:
         counter_show['from-landing'] += 1
-        counter_click['from-landing'] += 1
     else:
         pass
     return render_to_response('index.html')
 
 
 def landing(request):
-    global counter_show, counter_click
-
     if 'ab-test-arg' in request.GET['app/landing_alternate.html' or template_original]:
-        counter_show[template_test] += 1
         counter_click[template_test] += 1
     else:
         counter_click[template_original] += 1
-        counter_show[template_original] += 1
     return render_to_response(template_original, template_test)
 
 
 def stats(request):
     original = counter_click[template_original] / counter_show[template_original]
     test = counter_click[template_test] / counter_show[template_test]
-    if original or test in request.GET('marker'):
+    if original or test != 0:
         return render_to_response('stats.html', context={
-            'test_conversion': 0.5,
-            'original_conversion': 0.4,
+            'test': test,
+            'original': original,
         })
+    else:
+        return render_to_response('stats.html', context={'text':
+                                                             'Нет переходов',
+                                                         }
+                                  )
