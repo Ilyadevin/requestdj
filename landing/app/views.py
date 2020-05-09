7e7315a2320ddd41e2316c01b7c228a52fba51e0
Ilya Erlingas <Aligruit@gmail.com>
2020-05-09 13:55:35 +0300

    updating view

--- landing/app/views.py
+++ landing/app/views.py
@@ -8,36 +8,29 @@ template_original = 'landing.html'
 template_test = 'app/landing_alternate.html'
 
 
-# Александр, здравствуйте, забыл написать в сообщении коммент.
-# с возвращением шаблонов - мне в данном случае нужно возвращать два шаблона "test" & "original"?
 def index(request):
-    if 'from-landing' in request.GET:
-        counter_show['from-landing'] += 1
-        return render_to_response(template_original)
-    elif 'from-landing' not in request.GET:
-        return render_to_response(template_test)
-    else:
-        pass
+    counter_show[request.GET['from-landing']] += 1
     return render_to_response('index.html')
 
 
 def landing(request):
-    if 'ab-test-arg' in request.GET:
-        ab_test = request.GET['app/landing_alternate.html']
+    if request.GET['ab-test-arg']:
         counter_click[template_test] += 1
-    else:
+        return render_to_response(template_test)
+    elif not request.GET['ab-test-arg']:
         counter_click[template_original] += 1
-    return render_to_response(template_original, template_test)
+        return render_to_response(template_original)
+    else:
+        pass
 
 
-# Правильно ли я поступил с переменными в методе stats?
 def stats(request):
     global test, original
     try:
         original = counter_click[template_original] / counter_show[template_original]
         test = counter_click[template_test] / counter_show[template_test]
     except ZeroDivisionError as error:
-        if error is True:
+        if error is ZeroDivisionError:
             return render_to_response('stats.html', context={'text': 'Нет переходов',
                                                              }
                                       )
