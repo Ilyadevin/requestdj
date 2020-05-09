d08ae94d88d18d59afd5151be2b894dd21a89436
Ilya Erlingas <Aligruit@gmail.com>
2020-05-09 14:00:54 +0300

    updating urls.py

--- landing/app/urls.py
+++ landing/app/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from app.views import landing, stats, index
+from landing.app.views import landing, stats, index
 
 
 urlpatterns = [
     path('', index, name='index'),
     path('landing/', landing, name='landing'),
     path('stats/', stats, name='stats'),
-    path()
 ]
