2625b5de94b284bfd209afe10a96b2b4267d5c99
Ilya Erlingas <Aligruit@gmail.com>
2020-05-16 23:20:53 +0300

    done

--- pagination/app/views.py
+++ pagination/app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render_to_response, redirect
+from django.shortcuts import redirect, render
 from django.urls import reverse
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator
 import csv
 
 
@@ -13,26 +13,20 @@ def bus_stations(request):
     with open('data-398-2018-08-30.csv', 'r', encoding='cp1251') as csv_file:
         reader = csv.DictReader(csv_file)
         for row in reader:
-            stations_info.append({'Name': f"{row['StationName']}, {row['Direction']}", 'Street': row['Street'],
-                                  'District': row['District']})
+            stations_info.append({'Name': f"{row['StationName']}, {row['Street']} ({row['Direction']})",
+                                  'Street': row['Street'], 'District': row['District']})
     paginator = Paginator(stations_info, 15)
-    print(paginator.count)
     current_page = request.GET.get('page', 1)
-    try:
-        stations = paginator.get_page(current_page)
-    except PageNotAnInteger:
-        stations = paginator.page(number=1)
-    except EmptyPage:
-        stations = paginator.page(paginator.num_pages)
-    prev_page, next_page = None, None
+    stations = paginator.get_page(current_page)
+    next_page, prev_page = None, None
+    if stations.has_next():
+        next_page = stations.next_page_number()
     if stations.has_previous():
         prev_page = stations.previous_page_number()
-    elif stations.has_next():
-        next_page = stations.next_page_number()
     context = {
         'bus_stations': stations,
         'current_page': current_page,
-        'prev_page_url': prev_page,
-        'next_page_url': next_page,
+        'prev_page_url': f'?page_prev={prev_page}',
+        'next_page_url': f'?page={next_page}',
     }
-    return render_to_response('index.html', context=context)
+    return render(request, 'index.html', context=context)
